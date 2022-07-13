59dd08c6e63a30076b3ed5c87144330383083dc0
RacketyWater7 <haseebudeen@outlook.com>
2022-07-13 20:11:17 +0500

    made a slight change

--- client/src/utils/shortenAddress.js
+++ client/src/utils/shortenAddress.js
@@ -1,2 +1,4 @@
 export const shortenAddress = (address) =>
-  `${address.slice(0, 5)}...${address?.slice(address.length - 4)}`;
+  address
+    ? `${address?.slice(0, 5)}...${address?.slice(address.length - 4)}`
+    : `...`;
